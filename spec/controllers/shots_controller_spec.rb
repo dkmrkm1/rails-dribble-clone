8bf63facc346c0a094554742bf1ddb97676ee7c4
Duuun <daimura10@gmail.com>
2019-10-31 01:25:34 +0900

    rspec init

---
+++ spec/controllers/shots_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+Rspec.describe "ShotsController", type: :controller do
+  describe "GET #index" do
+    it "hoge" do
+      shot = create(:shot)
+      get :index
+    end
+  end
+end
