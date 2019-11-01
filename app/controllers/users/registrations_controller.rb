29713e1e230e7af58d4b204cba8a0404ff7b007a
Duuun <daimura10@gmail.com>
2019-11-01 15:36:35 +0900

    Update routing for devise（https://github.com/Duuun/rails-dribble-clone/issues/10）

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def detail
+    @user = User.find_by(id: params[:id])
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -50,10 +54,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    "/user/#{current_user.id}"
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
