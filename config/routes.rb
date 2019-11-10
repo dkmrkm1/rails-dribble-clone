4fa1e7d8c3dc6ab61013c2ed6360afd4a0ede50d
Duuun <daimura10@gmail.com>
2019-11-11 00:21:10 +0900

    Updated design and create registration users（https://github.com/Duuun/rails-dribble-clone/pull/27）

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   root "shots#index"
 
   resources :shots
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
